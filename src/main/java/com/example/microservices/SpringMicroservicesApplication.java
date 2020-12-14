bf79e49039c9bc0aa4075b70da065fa81e957a0f
GDM94 <giovanni.dimartino1994@gmail.com>
2020-12-14 09:48:36 +0100

    master pom jar

--- src/main/java/com/example/microservices/SpringMicroservicesApplication.java
+++ src/main/java/com/example/microservices/SpringMicroservicesApplication.java
@@ -3,11 +3,10 @@ package com.example.microservices;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class SpringMicroservicesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringMicroservicesApplication.class, args);
+
 	}
 
 }
